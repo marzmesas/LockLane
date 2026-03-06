17320827d1396ab2e321b4bc14f1de77247e5736
Mario Mesas <mmesasro@gmail.com>
2026-03-06 09:08:26 +0100

    feat(plugin): wire toolbar and notifications into tool window
    LocklaneToolWindowFactory builds DefaultActionGroup toolbar with all
    actions and wraps panel in BorderLayout. Registers Locklane notification
    group for balloon notifications on apply success and critical errors.

--- plugin/src/main/kotlin/io/locklane/LocklaneToolWindowFactory.kt
+++ plugin/src/main/kotlin/io/locklane/LocklaneToolWindowFactory.kt
@@ -1,19 +1,45 @@
 package io.locklane
 
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.actionSystem.Separator
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
+import io.locklane.action.ApplyPlanAction
+import io.locklane.action.RunPlanAction
+import io.locklane.action.SelectManifestAction
+import io.locklane.action.VerifyPlanAction
 import io.locklane.ui.LocklanePanel
+import java.awt.BorderLayout
+import javax.swing.JPanel
 
 class LocklaneToolWindowFactory : ToolWindowFactory {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val panel = LocklanePanel(project)
-        val content = ContentFactory.getInstance().createContent(panel, "", false)
+
+        val actionGroup = DefaultActionGroup().apply {
+            add(SelectManifestAction())
+            add(Separator())
+            add(RunPlanAction())
+            add(VerifyPlanAction())
+            add(ApplyPlanAction())
+        }
+
+        val toolbar = ActionManager.getInstance()
+            .createActionToolbar("LocklaneToolbar", actionGroup, true)
+        toolbar.targetComponent = panel
+
+        val wrapper = JPanel(BorderLayout()).apply {
+            add(toolbar.component, BorderLayout.NORTH)
+            add(panel, BorderLayout.CENTER)
+        }
+
+        val content = ContentFactory.getInstance().createContent(wrapper, "", false)
         toolWindow.contentManager.addContent(content)
     }
 
     override fun shouldBeAvailable(project: Project): Boolean = true
 }
-
